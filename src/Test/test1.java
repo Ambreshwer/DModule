0f5f0879d3e5a89dde68730baff922ea60984952
surya <surya@LAPTOP-36J5KNKJ>
2024-11-03 11:09:08 +0530

    03-11-First committ.....

---
+++ src/Test/test1.java
@@ -0,0 +1,16 @@
+package Test;
+
+public class test1 {
+
+	public static void main(String[] args) {
+		
+		div();
+
+	}
+
+	private static void div() {
+		
+		System.out.println("Skelton created");
+		
+	}
+}
